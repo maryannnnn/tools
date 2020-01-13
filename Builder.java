4cb777721a1a0fb8d67e11f0d4246bab03e028f4
Mark737 <50994527+maryannnnn@users.noreply.github.com>
2020-01-13 09:00:29 +0300

    Update Builder.java

--- Builder.java
+++ Builder.java
@@ -1,1 +1,28 @@
+package GitarsApp;
 
+public enum Builder {
+    FENDER, MARTIN, GIBSON, COLLINGS,
+    OLSON, RYAN, PRS, ANY;
+
+    public String toString() {
+        switch (this) {
+            case FENDER:
+                return "FENDER";
+            case MARTIN:
+                return "MARTIN";
+            case GIBSON:
+                return "GIBSON";
+            case COLLINGS:
+                return "COLLINGS";
+            case OLSON:
+                return "OLSON";
+            case RYAN:
+                return "RYAN";
+            case PRS:
+                return "PRS";
+            case ANY:
+                return "ANY";
+        }
+        return null;
+    }
+}
